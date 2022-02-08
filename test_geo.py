ceb901affaa2d3a413591779f123d9dbf565fc22
Ruoyao Wang <rw680@cam.ac.uk>
2022-02-08 16:47:04 +0000

    Added test functions to geo.py

---
+++ test_geo.py
@@ -0,0 +1,34 @@
+from typing import Set
+from floodsystem.stationdata import build_station_list
+import floodsystem.geo as geo
+
+"""Since data are in real-time, only the type of output
+or the possiblity for calling the function can be tested"""
+
+def test_stations_by_distance():
+    stations = build_station_list()
+    p = tuple((52.2053, 0.1218))
+    test = geo.stations_by_distance(stations, p)
+    assert type(test) == list
+
+def test_stations_within_radius():
+    stations = build_station_list()
+    centre = tuple((52.2053, 0.1218))
+    r = 10.0
+    test = geo.stations_within_radius(stations, centre, r)
+    assert type(test) == list
+    
+def test_rivers_with_station(): 
+    stations = build_station_list()
+    test = geo.rivers_with_station(stations)
+    assert type(test) == set
+
+def test_stations_by_river(): 
+    stations = build_station_list()
+    test = geo.stations_by_river(stations)
+    assert type(test) == dict
+
+def test_rivers_by_station_number(): 
+    stations = build_station_list()
+    test = geo.rivers_by_station_number(stations, 1)
+    assert type(test) == list
