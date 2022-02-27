71b0f97d033635d931a1782265943521ba2239f4
Ruoyao Wang <rw680@cam.ac.uk>
2022-02-27 23:22:01 +0000

    Added function for 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,4 +6,16 @@ def stations_level_over_threshold(stations, tol):
     for station in stations:
         if (station.relative_water_level() != None) and (station.relative_water_level() > tol):
             res.append(tuple((station.name, station.relative_water_level())))
+            res.sort(key = lambda x:x[1], reverse=True)
     return res
+
+#Task 2C
+def stations_highest_rel_level(stations, N):
+    """Returns a list of N stations with highest water level relative to typical range"""
+    res  = []
+    count = 0
+    for station in stations:
+        if station.relative_water_level() != None:
+            res.append(tuple((station.name, station.relative_water_level())))
+        res.sort(key = lambda x:x[1], reverse=True)
+    return res[:N]
