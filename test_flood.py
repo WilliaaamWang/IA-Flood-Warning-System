2ee194fe1fc6916f81328e8c0a92fffdd3f9fb4b
Ruoyao Wang <rw680@cam.ac.uk>
2022-03-02 16:37:26 +0000

    Extended means of testing

--- test_flood.py
+++ test_flood.py
@@ -6,9 +6,12 @@ def test_stations_level_over_threshold():
     update_water_levels(stations)
     test = flood.stations_level_over_threshold(stations, 1)
     assert(type(test)) == list
+    for i in test:
+        assert i[1] > 1
 
 def test_stations_highest_rel_level():
     stations = build_station_list()
     update_water_levels(stations)
     test = flood.stations_highest_rel_level(stations, 12)
     assert(type(test)) == list
+    assert len(test) == 12
