3ff575c6436ad458e50ce8bf30bc319e1c0e8086
LuranWang <67373568+LuranWang@users.noreply.github.com>
2022-03-02 03:00:19 +0000

    Tasks E, F, G

---
+++ Task2E.py
@@ -0,0 +1,24 @@
+import datetime
+from floodsystem.plot import plot_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+stations = build_station_list()
+update_water_levels(stations)
+res = []
+for station in stations:
+    if (station.relative_water_level() != None):
+        res.append(tuple((station, station.relative_water_level())))
+        res.sort(key = lambda x:x[1], reverse=True)
+stations_high = res[1:6]
+greatest_level_stations = []
+for tuples in stations_high:
+    greatest_level_stations.append(tuples[0])
+dates = []
+levels = []
+for item in greatest_level_stations:
+    date, level = fetch_measure_levels(
+        item.measure_id, dt=datetime.timedelta(days=10))
+    dates.append(date)
+    levels.append(level)
+plot_water_levels(greatest_level_stations, dates, levels)
