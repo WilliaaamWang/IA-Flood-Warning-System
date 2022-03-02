27df6d221d1402d182d31770e6f32b31015b2f63
LuranWang <67373568+LuranWang@users.noreply.github.com>
2022-03-02 23:55:18 +0000

    change 2E 2F 2G

--- Task2G.py
+++ Task2G.py
@@ -2,7 +2,6 @@ import datetime
 import numpy as np
 import matplotlib
 from floodsystem.analysis import polyfit
-from floodsystem.plot import plot_water_level_with_fit
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list, update_water_levels
 
@@ -10,25 +9,45 @@ stations = build_station_list()
 update_water_levels(stations)
 dt = 5
 compare_list = []
-res = []
+town_appearance = []
+town_list = {}
+town_score = []
 for station in stations:
-    if (station.relative_water_level() != None):
-        res.append(station)
-        res.sort(key = lambda x:x.relative_water_level(), reverse=True)
-stations_high = res[1:10]
-for station in stations_high:
-    date, level = fetch_measure_levels(
-        station.measure_id, dt=datetime.timedelta(days=dt))
-    poly, d = polyfit(date, level, 4)
-    if poly != None:
-        x = matplotlib.dates.date2num(date)
-        noise_distribution = np.array(level) - poly(x - d)
-        variance = np.var(noise_distribution)
-        prediction = poly(x[-1] + 1 - d)
-        result = prediction + 0.5 * variance
-        compare_list.append(tuple((station, result)))
+    if station.town not in town_appearance:
+        town_appearance.append(station.town)
+        town_list[station.town] = [station]
+    else:
+        town_list[station.town].append(station)
+for town in town_list:
+    score = 0
+    station_list = town_list[town]
+    res = []
+    for station in station_list:
+        if (station.relative_water_level() != None):
+            res.append(station)
+    for station in res:
+        date, level = fetch_measure_levels(
+            station.measure_id, dt=datetime.timedelta(days=dt))
+        if date != None and type(level) != float:
+            try: 
+                poly, d = polyfit(date, level, 4)
+                if poly != None:
+                    x = matplotlib.dates.date2num(date)
+                    noise_distribution = np.array(level) - poly(x - d)
+                    variance = np.var(noise_distribution)
+                    prediction = poly(x[-1] + 1 - d)
+                    result = prediction + 0.5 * variance
+                    score += result
+            except(TypeError):
+                score += 0
+        else:
+            score += 0
+    if len(res) == 0:
+        pass
+    else:
+        town_score.append([town, score/len(res)])       
 
-compare_list.sort(key = lambda x:x[1], reverse=True)
+town_score.sort(key = lambda x: x[1], reverse=True) 
 
-print(compare_list[0][0].name)
+print(town_score[0][0])
 
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -8,12 +8,19 @@ def plot_water_levels(stations, dates, levels):
     
     for i in range(len(stations)):
         plt.subplot(2, 3, i+1)
-        plt.plot(dates[i], levels[i])
-
+        x = matplotlib.dates.date2num(dates[i])
+        low_range = stations[i].typical_range[0]
+        high_range = stations[i].typical_range[1]
+        high_ranges = [high_range for i in range(len(x))]
+        low_ranges = [low_range for i in range(len(x))]
+        xs = np.linspace(x[0], x[-1], len(x))
+        plt.plot(x, levels[i])
+        plt.plot(xs, high_ranges, label ='relative_high')
+        plt.plot(xs, low_ranges, label ='relative_low')
         plt.xlabel('dates')
         plt.ylabel('water level (m)')
         plt.xticks(rotation=45)
-        plt.title("Station A")
+        plt.title(stations[i].name)
 
         plt.tight_layout()  # This makes sure plot does not cut off date labels
 
@@ -28,6 +35,13 @@ def plot_water_level_with_fit(stations, dates, levels, p):
         x = matplotlib.dates.date2num(dates[i])
         plt.plot(x, levels[i], '.')
         x1 = np.linspace(x[0], x[-1], 30)
+        low_range = stations[i].typical_range[0]
+        high_range = stations[i].typical_range[1]
+        high_ranges = [high_range for i in range(len(x))]
+        low_ranges = [low_range for i in range(len(x))]
+        xs = np.linspace(x[0], x[-1], len(x))
+        plt.plot(xs, high_ranges, label ='relative_high')
+        plt.plot(xs, low_ranges, label ='relative_low')
         plt.plot(x1, poly(x1-d))
         plt.title(stations[i].name)
 
